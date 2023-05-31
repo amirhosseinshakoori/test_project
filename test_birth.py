602ef01e085588816aeb5c2b97c04dd4998fe1b4
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2023-05-31 22:37:01 +0330

    birth
    Calculations for months and discounts

---
+++ test_birth.py
@@ -0,0 +1,19 @@
+import unittest
+from datetime import datetime, date
+from birth import Discount
+
+class DiscountTest(unittest.TestCase):
+    def setUp(self):
+        birth_date = date(datetime.now().year, 5, 31)
+        registration_date = date(2022, 1, 15)
+        self.discount = Discount("John Doe", birth_date, registration_date)
+
+    def test_birthday_discount(self):
+        expected_discount = 0.5
+
+        discount_rate = self.discount.apply_discount()
+
+        self.assertEqual(discount_rate, expected_discount)
+
+if __name__ == '__main__':
+    unittest.main()
