8512d32d8d2707bf648930d5c15098ef8d7e57b7
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2023-05-31 23:51:04 +0330

    Update test_birth.py
    write new test for birth

--- test_birth.py
+++ test_birth.py
@@ -1,19 +1,39 @@
 import unittest
-from datetime import datetime, date
+from datetime import date
 from birth import Discount
 
 class DiscountTest(unittest.TestCase):
-    def setUp(self):
-        birth_date = date(datetime.now().year, 5, 31)
-        registration_date = date(2022, 1, 15)
-        self.discount = Discount("mohammad goodarzi", birth_date, registration_date)
-
     def test_birthday_discount(self):
-        expected_discount = 0.5
+        # Set up
+        today = date.today()
+        username = "JohnDoe"
+        birth_date = date(today.year, today.month, today.day)  # Same birth date as today
+        registration_date = date(2022, 1, 1)  # A specific registration date
+        discount = Discount(username, birth_date, registration_date)
 
-        discount_rate = self.discount.apply_discount()
+        # Apply discount
+        result = discount.apply_discount()
 
-        self.assertEqual(discount_rate, expected_discount)
+        # Check if the discount rate is correct
+        self.assertEqual(result, 0.5)
+
+    def test_membership_discount(self):
+        # Set up
+        today = date.today()
+        username = "JaneSmith"
+        birth_date = date(2000, 5, 1)  # A specific birth date
+        registration_date = date(today.year, today.month, today.day)  # Same registration date as today
+        discount = Discount(username, birth_date, registration_date)
+
+        # Apply discount
+        result = discount.apply_discount()
+
+        # Calculate the expected discount rate
+        months_as_member = (today.year - registration_date.year) * 12 + (today.month - registration_date.month)
+        expected_discount_rate = min(months_as_member * 0.05, 0.3)
+
+        # Check if the discount rate is correct
+        self.assertEqual(result, expected_discount_rate)
 
 if __name__ == '__main__':
     unittest.main()
